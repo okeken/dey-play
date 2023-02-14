fb7e798d1910d587630b65cd8dce487a99cbbb68
oke kehinde <okekehinde@gmail.com>
2023-02-14 02:30:27 +0100

    misc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
 import type { NextPage } from "next";
+import React from "react";
 import Iframe from "react-iframe";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex justify-center mt-12">
-      <Iframe
-        url="https://multi-step-okeken.vercel.app/multi-step"
-        width="640px"
-        height="920px"
-        id=""
-        className=""
-        display="block"
-        position="relative"
-      />
-    </div>
+    <>
+      <div className="flex justify-center mt-12 overflow-hidden border">
+        <Iframe
+          url="https://multi-step-okeken.vercel.app/multi-step"
+          // url="http://localhost:3000/multi-step"
+          width="640px"
+          height="920px"
+          id=""
+          className="overflow-hidden"
+        />
+      </div>
+    </>
   );
 };
 
